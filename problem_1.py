2f9d31822ec92a66a20e838c990c1b336e3388c2
codereena <lbu279@bu.edu>
2026-09-05 21:07:39 +0000

    week 1 homework first draft

---
+++ problem_1.py
@@ -0,0 +1,7 @@
+my_int = 27
+my_float = 900.12
+my_str = "chocolate"
+
+print("My favorite number is", my_int)
+print("My favorite float is", my_float)
+print("My favorite ice cream flavor is", my_str)
